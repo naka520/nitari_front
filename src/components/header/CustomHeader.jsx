137642bf42b0856368e4373f5d76985571ff555b
naka520 <yashipipi0520@gmail.com>
2023-08-27 10:56:19 +0900

    [fix]header

--- src/components/header/CustomHeader.jsx
+++ src/components/header/CustomHeader.jsx
@@ -6,14 +6,14 @@ function HamburgerMenu() {
 
   return (
     <div className="relative">
-      <button onClick={() => setIsOpen(!isOpen)} className="bg-green400 focus:outline-none">
+      <button onClick={() => setIsOpen(!isOpen)} className="focus:outline-none">
         <div className="w-6 h-0.5 mb-1 bg-white"></div>
         <div className="w-6 h-0.5 mb-1 bg-white"></div>
         <div className="w-6 h-0.5 bg-white"></div>
       </button>
       {isOpen && (
-        <div className="absolute top-full left-0 w-48 bg-white rounded shadow-lg">
-          <Link to="/home" onClick={() => setIsOpen(false)} className="block px-4 py-2">ホーム画面</Link>
+        <div className="absolute top-full left-0 w-48  rounded shadow-lg">
+          <Link to="/home" onClick={() => setIsOpen(false)} className="block px-4 py-2 border-slate-300">ホーム画面</Link>
           <Link to="/" onClick={() => setIsOpen(false)} className="block px-4 py-2">アカウント連携</Link>
           <Link to="/" onClick={() => setIsOpen(false)} className="block px-4 py-2">SNS投稿</Link>
           {/* 他のリンクもこちらに追加 */}
