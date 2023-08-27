b5683fad2876561d2f31520d88fd1af1dbedf23b
naka520 <yashipipi0520@gmail.com>
2023-08-27 10:32:07 +0900

    [fix]ハンバーガーメニューのバグを修正

--- src/components/header/CustomHeader.jsx
+++ src/components/header/CustomHeader.jsx
@@ -1,18 +1,12 @@
-import HamburgerMenu from '../../components/HamburgerMenu'; // HamburgerMenu コンポーネントをインポート
+import HamburgerMenu from '../../components/HamburgerMenu';
 
 function Header() {
   return (
-    <div className="flex items-center justify-between bg-white p-4 shadow-lg">
-      <div className="flex items-center">
-        {/* ハンバーガーメニューを追加 */}
-        <HamburgerMenu />
-        
-        {/* サイトのタイトル */}
-        <h1 className="text-black text-4xl font-playfair transform rotate-3">Nippoちゃん</h1>
-      </div>
-
-      {/* 他のヘッダーコンテンツ（例えば、検索バー、ログインボタンなど） */}
-    </div>
+    <header className="flex items-center justify-between p-4 bg-white">
+      <HamburgerMenu />
+      <h1 className="text-4xl font-semibold text-black transform rotate-3">日報ちゃん</h1>
+      {/* 他のヘッダーコンテンツ */}
+    </header>
   );
 }
 
