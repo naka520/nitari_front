5e499557199cc3bdaaf0c1f29a2492adb372aca4
naka520 <yashipipi0520@gmail.com>
2023-08-27 10:41:14 +0900

    [fix]ハンバーガーメニューとヘッダーを結合

--- src/components/header/CustomHeader.jsx
+++ src/components/header/CustomHeader.jsx
@@ -1,4 +1,27 @@
-import HamburgerMenu from '../../components/HamburgerMenu';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+function HamburgerMenu() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="relative">
+      <button onClick={() => setIsOpen(!isOpen)} className="focus:outline-none">
+        <div className="w-6 h-0.5 mb-1 bg-white"></div>
+        <div className="w-6 h-0.5 mb-1 bg-white"></div>
+        <div className="w-6 h-0.5 bg-white"></div>
+      </button>
+      {isOpen && (
+        <div className="absolute top-full left-0 w-48 bg-white rounded shadow-lg">
+          <Link to="/home" onClick={() => setIsOpen(false)} className="block px-4 py-2">ホーム画面</Link>
+          <Link to="/" onClick={() => setIsOpen(false)} className="block px-4 py-2">アカウント連携</Link>
+          <Link to="/" onClick={() => setIsOpen(false)} className="block px-4 py-2">SNS投稿</Link>
+          {/* 他のリンクもこちらに追加 */}
+        </div>
+      )}
+    </div>
+  );
+}
 
 function Header() {
   return (
