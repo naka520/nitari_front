a181635195d32a499e9dcd01ab344743485d8b09
naka520 <yashipipi0520@gmail.com>
2023-08-27 10:57:12 +0900

    [fix]ヘッダーバグ

--- src/components/header/CustomHeader.jsx
+++ src/components/header/CustomHeader.jsx
@@ -12,7 +12,7 @@ function HamburgerMenu() {
         <div className="w-6 h-0.5 bg-white"></div>
       </button>
       {isOpen && (
-        <div className="absolute top-full left-0 w-48  rounded shadow-lg">
+        <div className="absolute top-full left-0 w-48 bg-white rounded shadow-lg">
           <Link to="/home" onClick={() => setIsOpen(false)} className="block px-4 py-2 border-slate-300">ホーム画面</Link>
           <Link to="/" onClick={() => setIsOpen(false)} className="block px-4 py-2">アカウント連携</Link>
           <Link to="/" onClick={() => setIsOpen(false)} className="block px-4 py-2">SNS投稿</Link>
@@ -25,7 +25,7 @@ function HamburgerMenu() {
 
 function Header() {
   return (
-    <header className="flex items-center justify-between p-4 bg-white">
+    <header className="flex items-center justify-between p-4 ">
       <HamburgerMenu />
       <h1 className="text-4xl font-semibold text-black transform rotate-3">日報ちゃん</h1>
       {/* 他のヘッダーコンテンツ */}
